b2c465b5a6734eab712ffda4b562c4b0bbc9a092
Elias Taveras <eliastave12@gmail.com>
2020-03-26 23:41:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -157,6 +157,19 @@ def shoe_size(players_name)
   end
 end
 
+def shoe_size(players_name)
+    game_hash.each do |court, player_team|
+      player_team.each do |team_info, player_info|
+        if team_info == :players
+          player_info.each do |player|
+            if player[:player_name] == players_name
+              return player[:shoe]
+            end
+        end
+      end
+    end
+  end
+end
 
 
 
