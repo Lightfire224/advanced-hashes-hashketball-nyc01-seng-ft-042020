6276a476b582cc5e2c1cba426492e0fd66906345
Elias Taveras <eliastave12@gmail.com>
2020-03-26 23:35:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -135,8 +135,7 @@ def num_points_scored(players_name)
       if team_info == :players
         player_info.each do |player|
           if player[:player_info] == players_name
-            return player
-          
+            return player[:points]
       end
     end
   end
